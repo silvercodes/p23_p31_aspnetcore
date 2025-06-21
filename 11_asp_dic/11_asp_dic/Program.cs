98ec39ff7978e7a279cb46ce2405782e98908d9e
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-06-21 14:32:33 +0300

    add: services

--- 11_asp_dic/11_asp_dic/Program.cs
+++ 11_asp_dic/11_asp_dic/Program.cs
@@ -32,7 +32,6 @@
 
 #endregion
 
-
 #region Example_1
 //var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddScoped<IRandomService, RandomService>();
@@ -84,49 +83,323 @@
 
 #endregion
 
+#region AddHttpClient()
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddHttpClient();
+//builder.Services.AddSingleton<DataService>();
+
+//var app = builder.Build();
+
+//app.MapGet("/", async (HttpContext ctx, DataService service) =>
+//{
+//    return await service.GetData();
+//});
+
+//app.Run();
+
+//class DataService
+//{
+//    private readonly HttpClient httpClient;
+
+//    public DataService(HttpClient httpClient)
+//    {
+//        this.httpClient = httpClient;
+//    }
+
+//    public async Task<string> GetData()
+//    {
+//        var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+//        return await response.Content.ReadAsStringAsync();
+//    }
+//}
+
+
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddHttpClient("jsonph", client =>
+//{
+//    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+//    client.DefaultRequestHeaders.Add("User-Agent", "MyApp");
+//    client.Timeout = TimeSpan.FromSeconds(15);
+//});
+//builder.Services.AddSingleton<DataService>();
+
+//var app = builder.Build();
+
+//app.MapGet("/", async (HttpContext ctx, DataService service) =>
+//{
+//    return await service.GetData();
+//});
+
+//app.Run();
+
+//class DataService
+//{
+//    private readonly IHttpClientFactory cFactory;
+
+//    public DataService(IHttpClientFactory cFactory)
+//    {
+//        this.cFactory = cFactory;
+//    }
+
+//    public async Task<string> GetData()
+//    {
+//        var client = cFactory.CreateClient("jsonph");
+//        var response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+//        return await response.Content.ReadAsStringAsync();
+//    }
+//}
+
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddSingleton<DataService>();
+
+//builder.Services.AddHttpClient<DataService>(client =>
+//{
+//    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+//    client.DefaultRequestHeaders.Add("Accept", "application/json");
+//    client.Timeout = TimeSpan.FromSeconds(15);
+//});
+
+
+//var app = builder.Build();
+
+//app.MapGet("/", async (HttpContext ctx, DataService service) =>
+//{
+//    return await service.GetData();
+//});
+
+//app.Run();
+
+//class DataService
+//{
+//    private readonly HttpClient httpClient;
+
+//    public DataService(HttpClient httpClient)
+//    {
+//        this.httpClient = httpClient;
+//    }
+
+//    public async Task<string> GetData()
+//    {
+//        return await httpClient.GetStringAsync("users");
+//    }
+//}
+
+
+#endregion
 
 #region Example_2
 
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddScoped<IUserService, UserService>();
+
+//builder.Services.AddHttpClient<IUserApiClient, UserApiClient>(client =>
+//{
+//    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+//});
+
+//var app = builder.Build();
+
+
+//app.MapGet("/users", async (HttpContext ctx, IUserService userService) =>
+//{
+//    await ctx.Response.WriteAsJsonAsync(await userService.GetUsersAsync());
+
+//    // return await userService.GetUsersAsync();
+//});
+
+//app.Run();
+
+
+//// ------
+//public interface IUserService
+//{
+//    Task<User[]?> GetUsersAsync();
+//}
+//public class UserService(IUserApiClient api) : IUserService
+//{
+//    public async Task<User[]?> GetUsersAsync() =>
+//        await api.GetUsersAsync();
+//}
+//// ------
+//public interface IUserApiClient
+//{
+//    Task<User[]?> GetUsersAsync();
+//}
+//public class UserApiClient(HttpClient http) : IUserApiClient
+//{
+//    public async Task<User[]?> GetUsersAsync() =>
+//        await http.GetFromJsonAsync<User[]>("/users");
+//}
+
+//public record class User(int Id, string Name, string Username, string Email);
+
+#endregion
+
+#region Get services
+// --- Use ctor
+
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddSingleton<IRandomService, RandomService>();
+//builder.Services.AddSingleton<MyService>();
+
+//var app = builder.Build();
+
+//app.Run();
+
+//public class MyService
+//{
+//    private readonly IRandomService randomService;
+
+//    public MyService(IRandomService randomService)
+//    {
+//        this.randomService = randomService;
+//    }
+//}
+
+
+
+// --- Use parameters
+
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddSingleton<IRandomService, RandomService>();
+//builder.Services.AddSingleton<MyService>();
+
+//var app = builder.Build();
+
+//app.MapGet("/", async (HttpContext ctx, MyService service) =>
+//{
+//    await ctx.Response.WriteAsync(service.GetVal().ToString());
+//});
+
+//app.Run();
+
+//public class MyService
+//{
+//    private readonly IRandomService randomService;
+
+//    public MyService(IRandomService randomService)
+//    {
+//        this.randomService = randomService;
+//    }
+//    public int GetVal() => randomService.NextValue(1, 100);
+//}
+
+
+// ---- Ручное получение из HttpContext
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddSingleton<IRandomService, RandomService>();
+//var app = builder.Build();
+
+//app.MapGet("/manual", async (HttpContext ctx) =>
+//{
+//    var rnd = ctx.RequestServices.GetRequiredService<IRandomService>();
+//    await ctx.Response.WriteAsync(rnd.NextValue(1, 100).ToString());
+//});
+
+//app.Run();
+
+
+// --- Получение после app.Build()
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddSingleton<IRandomService, RandomService>();
+//var app = builder.Build();
+
+//using(var scope = app.Services.CreateScope())
+//{
+//    var scopedService = scope.ServiceProvider.GetRequiredService<IRandomService>();
+//    Console.WriteLine(scopedService.NextValue(1, 100));
+//}
+
+//app.Run();
+
+
+
+
+//public interface IRandomService
+//{
+//    int NextValue(int min, int max);
+//}
+//public class RandomService : IRandomService
+//{
+//    private readonly Random random = new Random();
+//    public int NextValue(int min, int max)
+//    {
+//        return random.Next(min, max);
+//    }
+//}
+
+#endregion
+
+#region service LifeCycle
+
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddScoped<IUserService, UserService>();
-
-builder.Services.AddHttpClient(client =>
-{
-    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
-});
+builder.Services.AddSingleton<SingletonService>();
+builder.Services.AddScoped<ScopedService>();
+builder.Services.AddTransient<TransientService>();
 
 var app = builder.Build();
 
-app.MapGet("/users", async (HttpContext ctx, IUserService userService) =>
-{
-    await ctx.Response.WriteAsJsonAsync(await userService.GetUsersAsync());
+app.MapGet("/", (
+    SingletonService singletonService_1,
+    SingletonService singletonService_2,
+    ScopedService scopedService_1,
+    ScopedService scopedService_2,
+    TransientService transientService_1,
+    TransientService transientService_2
+) => {
+    return new
+    {
+        Singleton = new
+        {
+            Id1 = singletonService_1.Id,
+            Id2 = singletonService_2.Id,
+            Created = singletonService_1.Created,
+            Equal = singletonService_1.Id == singletonService_2.Id
+        },
+        Scoped = new
+        {
+            Id1 = scopedService_1.Id,
+            Id2 = scopedService_2.Id,
+            Created = scopedService_1.Created,
+            Equal = scopedService_1.Id == scopedService_2.Id
+        },
+        Transient = new
+        {
+            Id1 = transientService_1.Id,
+            Id2 = transientService_2.Id,
+            Created1 = transientService_1.Created,
+            Created2 = transientService_2.Created,
+            Equal = transientService_1.Id == transientService_2.Id
+        },
 
-    // return await userService.GetUsersAsync();
+    };
 });
 
 app.Run();
 
-
-
-public interface IUserService
+public class SingletonService
 {
-    Task<User[]> GetUsersAsync();
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Created { get; } = DateTime.Now.ToString("HH:mm:ss.fff");
 }
-public class UserService : IUserService
+public class ScopedService
 {
-    public Task<User[]> GetUsersAsync()
-    {
-        throw new NotImplementedException();
-    }
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Created { get; } = DateTime.Now.ToString("HH:mm:ss.fff");
 }
-
-
-
-
-public record class User(int Id, string Name, string Username, string Email);
+public class TransientService
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Created { get; } = DateTime.Now.ToString("HH:mm:ss.fff");
+}
 
 #endregion
-
-
-
